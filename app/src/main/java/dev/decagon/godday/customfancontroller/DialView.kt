2ac646fd97c01e9b735e01f84f5201d9abfc5ce2
Godday Okoduwa <goddayokos@gmail.com>
2021-10-11 14:21:26 +0100

    Adding Accessibility to DailView

--- app/src/main/java/dev/decagon/godday/customfancontroller/DialView.kt
+++ app/src/main/java/dev/decagon/godday/customfancontroller/DialView.kt
@@ -58,13 +58,15 @@ class DialView @JvmOverloads constructor(
             fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
             fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
         }
+
+        updateContentDescription()
     }
 
     override fun performClick(): Boolean {
         if (super.performClick()) return true
 
         fanSpeed = fanSpeed.next()
-        contentDescription = resources.getString(fanSpeed.label)
+        updateContentDescription()
 
         invalidate()
         return true
@@ -110,5 +112,9 @@ class DialView @JvmOverloads constructor(
             val label = resources.getString(i.label)
             canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
         }
+    }
+
+    fun updateContentDescription() {
+        contentDescription = resources.getString(fanSpeed.label)
     }
 }
